e2b19348af48c3039109c97ab68e0602e4d434c2
alikashi543211 <alikashi54321@gmail.com>
2023-07-19 09:06:48 +0500

    frontend theme flow set

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::prefix('auth')->group(function () {
+    Route::controller(AuthController::class)->group(function () {
+        Route::post('login', 'login');
+        Route::post('logout', 'logout');
+    });
+    Route::controller(RegisterController::class)->group(function () {
+        Route::post('register', 'register');
+        Route::post('verifyEmailVerificationCode', 'verifyEmailVerificationCode');
+        Route::post('verificationCodeResend', 'verificationCodeResend');
+    });
+});
